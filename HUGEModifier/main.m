4577c08af2ced48a323415834bfdf9fea0df35df
Hiroki Kawakami <40638139+Hiroki-Kawakami@users.noreply.github.com>
2020-09-17 06:43:33 +0900

    Initial Commit

---
+++ HUGEModifier/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HUGEModifier
+//
+//  Created by hiroki on 2020/09/17.
+//  Copyright © 2020 hiroki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
